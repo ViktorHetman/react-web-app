a260c05d9ccecf701acc21fed834241af07892e3
Viktor Hetman <vitya.getman.79@gmail.com>
2023-02-13 18:00:50 +0200

    adding cart component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 import Card from './Components/Card/Card';
+import Cart from './Components/Сart/Cart';
 import data from './db/db'
 
 const foods = data
 
 function App() {
+
+  const [cartItems, setCartItems] = useState([])
+
   return (
     <div className="App">
       <h1 className='heading'>Зробити замовлення</h1>
+      <Cart/>
         <div className='cards_container'>
           {foods.map((food) => {
             return <Card key={food.id} {...food}/>
